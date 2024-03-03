97dff69d983fd7ddcbf830c3c976f76c44941673
Luciano Blanco <luciano.blanco@live.u-tad.com>
2024-03-03 17:52:25 +0100

    ok prod.properties

---
+++ src/test/java/com/utad/apipizzas/ApiPizzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utad.apipizzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPizzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
